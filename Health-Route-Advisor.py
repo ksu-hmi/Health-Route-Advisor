a288b8e2d476e698641e9d3936a1e29b16fce2a9
hbodden123 <142348214+hbodden123@users.noreply.github.com>
2023-11-18 06:18:51 -0500

    Update Health-Route-Advisor.py
    Added program welcome message

---
+++ Health-Route-Advisor.py
@@ -0,0 +1,4 @@
+print('Welcome to Health Route Advisor, please briefly review the information below before you begin.')
+print('\n\nPrimary Care is intended for non-urgent conditions that can wait for a scheduled appointment')
+print('\n\nUrgent Care is an additional resource for if you cannot obtain a timely primary care visit and are experiencing a mild, non-life threatening illness or injury')
+print('\n\nEmergency room visits are for severe illness or injury with the potential to be life-threatening. Also, for any life-threatening symptoms to include chest pain, facial droop with slurred speech or weakness/numbness, difficulty breathing, uncontrolled breathing.')

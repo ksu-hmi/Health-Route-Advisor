83d16393582e6cc3a9f848f88466afed463cf7a4
hbodden123 <142348214+hbodden123@users.noreply.github.com>
2023-11-18 11:44:24 -0500

    Update Health-Route-Advisor.py
    Review of symptoms related to urgent care

--- Health-Route-Advisor.py
+++ Health-Route-Advisor.py
@@ -15,3 +15,14 @@ def advice():
         if emergency_room2.lower() == "y":
             print('Please seek care at an emergency room.')
         else:
+            fever = input("Do you have a mild fever without rash? (Y/N): ")
+            wheeze = input("Are you wheezing or slightly short of breath? (Y/N): ")
+            cut_small = input("Do you have a small cut or minor injury? (Y/N): ")
+            flu = input("Do you have symptoms of cold or flu? (Y/N): ")
+            vomiting = input("Are you experiencing nausea with vomiting? (Y/N): ")
+            diarrhea = input("Are you experiencing diarrhea? (Y/N): ")
+            urine = input("Are you having pain or difficulty with urinating? (Y/N): ")
+            bite = input("Do you a minor animal or insect bite? (Y/N): ")
+            allergy = input("Are you experiencing mild symptoms of allergic reaction(redness, itching, swelling, no difficulty breathing)? (Y/N): ")
+            if fever or wheeze or cut_small or flu or vomiting or diarrhea or urine or bite or allergy == "y":
+                print("Please seek treatment at an urgent care. Thank you for using Health Route Advisor. Get well soon!")
